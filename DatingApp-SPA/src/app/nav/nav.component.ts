3d19a9387bd4384566b97528093352a809d4e031
Igal Schwartz <IgalShwartz@gmail.com>
2020-01-22 10:39:11 +0200

    End of section 7

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +12,7 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) {
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) {
 
   }
 
@@ -25,6 +26,8 @@ export class NavComponent implements OnInit {
      }, error => {
        console.log(error);
        this.alertify.error(error);
+     }, () => {
+       this.router.navigate(['/members']);
      });
   }
 
@@ -36,5 +39,6 @@ export class NavComponent implements OnInit {
     localStorage.removeItem('token'); // just only remove this is the cause why we dont move to auth service
     console.log('Logged out');
     this.alertify.message('Logged out');
+    this.router.navigate(['/home']);
   }
 }
